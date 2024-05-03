91b9cdf2f0fa614e422586def51da05fb254ac1c
ayoubandroid1 <scarl4463@gmail.com>
2024-05-03 04:36:01 +0100

    changes made

---
+++ backend/dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors")); // Import cors from its package
+require("dotenv/config");
+const mongoose_1 = __importDefault(require("mongoose"));
+const users_1 = __importDefault(require("./routes/users"));
+const auth_1 = __importDefault(require("./routes/auth"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cloudinary_1 = require("cloudinary");
+const my_hotels_1 = __importDefault(require("../src/routes/my-hotels"));
+const hotels_1 = __importDefault(require("./routes/hotels"));
+const my_booking_1 = __importDefault(require("./routes/my-booking"));
+const path_1 = __importDefault(require("path"));
+cloudinary_1.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STRING);
+const app = (0, express_1.default)();
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({
+    extended: true
+}));
+app.use((0, cors_1.default)({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../../hotel-app/dist")));
+app.use("/api/auth", auth_1.default);
+app.use("/api/users", users_1.default);
+app.use("/api/my-hotels", my_hotels_1.default);
+app.use("/api/hotels", hotels_1.default);
+app.use("/api/my-bookings", my_booking_1.default);
+app.listen(7000, () => {
+    console.log("server is running on localhost:7000");
+});
---
+++ backend/dist/routes/hotels.js
@@ -0,0 +1,145 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const hotel_1 = __importDefault(require("../models/hotel"));
+const express_validator_1 = require("express-validator");
+const auth_1 = __importDefault(require("../middleware/auth"));
+const router = express_1.default.Router();
+router.get("/search", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const query = constructSearchQuery(req.query);
+        let sortOptions = {};
+        switch (req.query.sortOption) {
+            case "starRating":
+                sortOptions = { starRating: -1 };
+                break;
+            case "pricePerNightAsc":
+                sortOptions = { pricePerNight: 1 };
+                break;
+            case "pricePerNightDesc":
+                sortOptions = { pricePerNight: -1 };
+                break;
+        }
+        const pageSize = 5;
+        const pageNumber = parseInt(req.query.page ? req.query.page.toString() : "1");
+        const skip = (pageNumber - 1) * pageSize;
+        const hotels = yield hotel_1.default.find(query)
+            .sort(sortOptions)
+            .skip(skip)
+            .limit(pageSize);
+        const total = yield hotel_1.default.countDocuments(query);
+        const response = {
+            data: hotels,
+            pagination: {
+                total,
+                page: pageNumber,
+                pages: Math.ceil(total / pageSize),
+            },
+        };
+        res.json(response);
+    }
+    catch (error) {
+        console.log("error", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}));
+router.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const hotels = yield hotel_1.default.find().sort("-lastUpdated");
+        res.json(hotels);
+    }
+    catch (error) {
+        console.log("error", error);
+        res.status(500).json({ message: "Error fetching hotels" });
+    }
+}));
+router.get("/:id", [(0, express_validator_1.param)("id").notEmpty().withMessage("Hotel ID is required")], (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const errors = (0, express_validator_1.validationResult)(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const id = req.params.id.toString();
+    try {
+        const hotel = yield hotel_1.default.findById(id);
+        res.json(hotel);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error fetching hotel" });
+    }
+}));
+router.post("/:hotelId/bookings", auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const newBooking = Object.assign(Object.assign({}, req.body), { userId: req.userId });
+        const hotel = yield hotel_1.default.findOneAndUpdate({ _id: req.params.hotelId }, {
+            $push: { bookings: newBooking },
+        });
+        if (!hotel) {
+            return res.status(400).json({ message: "hotel not found" });
+        }
+        yield hotel.save();
+        res.status(200).send();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "something went wrong" });
+    }
+}));
+const constructSearchQuery = (queryParams) => {
+    let constructedQuery = {};
+    if (queryParams.destination) {
+        constructedQuery.$or = [
+            { city: new RegExp(queryParams.destination, "i") },
+            { country: new RegExp(queryParams.destination, "i") },
+        ];
+    }
+    if (queryParams.adultCount) {
+        constructedQuery.adultCount = {
+            $gte: parseInt(queryParams.adultCount),
+        };
+    }
+    if (queryParams.childCount) {
+        constructedQuery.childCount = {
+            $gte: parseInt(queryParams.childCount),
+        };
+    }
+    if (queryParams.facilities) {
+        constructedQuery.facilities = {
+            $all: Array.isArray(queryParams.facilities)
+                ? queryParams.facilities
+                : [queryParams.facilities],
+        };
+    }
+    if (queryParams.types) {
+        constructedQuery.type = {
+            $in: Array.isArray(queryParams.types)
+                ? queryParams.types
+                : [queryParams.types],
+        };
+    }
+    if (queryParams.stars) {
+        const starRatings = Array.isArray(queryParams.stars)
+            ? queryParams.stars.map((star) => parseInt(star))
+            : parseInt(queryParams.stars);
+        constructedQuery.starRating = { $in: starRatings };
+    }
+    if (queryParams.maxPrice) {
+        constructedQuery.pricePerNight = {
+            $lte: parseInt(queryParams.maxPrice).toString(),
+        };
+    }
+    return constructedQuery;
+};
+exports.default = router;
